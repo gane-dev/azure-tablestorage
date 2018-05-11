96023a3a1389915bdbd57a398577e40eaa485f50
ganesh devaraj <gadeva.mba@gmail.com>
2018-05-10 22:00:24 -0400

    Create queues in azure

---
+++ QueueExample.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace TableStorage
+{
+    class QueueExample
+    {
+        public static void AddQueue(CloudStorageAccount storageAccount)
+        {
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+             
+
+            CloudQueue queue = queueClient.GetQueueReference("queue");
+            queue.CreateIfNotExists();
+            queue.AddMessage(new CloudQueueMessage("Queued message 1"));
+            queue.AddMessage(new CloudQueueMessage("Queued message 2"));
+            queue.AddMessage(new CloudQueueMessage("Queued message 3"));
+        }
+    }
+}
